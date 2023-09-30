303297c1f01409bfc6de132fe9fa8ff0d441088b
lbonne <lucbonne@hotmail.com>
2023-09-30 17:30:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import math
 import pandas as pd
 import streamlit as st
 import datetime
+import yfinance as yf
 
 """
 # Welcome to Streamlit!
@@ -19,44 +20,16 @@ In the meantime, below is an example of what you can do with just a few lines of
 st.write("Yowkes!!!")
 mylist = st.secrets.YF.ThickerList
 st.write(mylist[0])
+symbol1 = yf.Ticker(mylist[0])
+st.write(symbol1.info)
 
 
 
 
-add_button = st.button("Lets go!")
-d = st.date_input("When's your birthday", datetime.date(2019, 7, 6))
-st.write('Your birthday is:', d)
 
-st.button("Reset", type="primary")
-if st.button('Say hello'):
-    st.write('Why hello there')
-else:
-    st.write('Goodbye')
-
-# Using "with" notation
 with st.sidebar:
     add_radio = st.radio(
         "Choose a shipping method",
         ("Standard (5-15 days)", "Express (2-5 days)")       
     )
     
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 75, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
